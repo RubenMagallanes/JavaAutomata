59481c47952c2025ef0ec4bf1417065be9d8a687
willhs <whardwicksmith@gmail.com>
2015-07-31 17:04:23 +1200

    added two-part curve for marker-mid (commented out)

--- src/web/js/viz.js
+++ src/web/js/viz.js
@@ -76,7 +76,10 @@
             var dx = d.target.x - d.source.x,
                 dy = d.target.y - d.source.y,
                 dr = Math.sqrt(dx * dx + dy * dy);
+                //return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + (d.source.x + (dx/2)) + "," + (d.source.y + (20))
+                //+ "M" + (d.source.x + (dx/2)) + "," + (d.source.y + (20)) + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
                 return "M" + d.source.x + "," + d.source.y + "A" + dr + "," + dr + " 0 0,1 " + d.target.x + "," + d.target.y;
+
         }
 
         function transform(d) {
