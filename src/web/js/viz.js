e7c5824e117714c7c538dfc7ff773131c373f2ea
willhs <whardwicksmith@gmail.com>
2015-07-31 16:48:54 +1200

    started show/hide labels

--- src/web/js/viz.js
+++ src/web/js/viz.js
@@ -3,9 +3,13 @@
 // http://bl.ocks.org/mbostock/1153292
 (function (self){
 
-    var states,
-        links;
+    var states, // nodes bound to program states
+        links, // links bound to state transitions
+        varsChosen = [], // variables to be shown
+        funcsChosen = [], // functions to be shown
+        svg; // the svg element to draw viz on
 
+    // force layout
     var force = d3.layout.force()
         .charge(-300)
         .size([500, 500])
@@ -15,9 +19,13 @@
     var colour = d3.scale.category20(),
         circleRad = 10;
 
-    self.init = function (data, svg){
+    // initialise the layout with data
+    self.init = function (data, _svg){
         states = data.states;
         links = data.links;
+        svg = _svg;
+
+        setChosenNames();
 
     	force.nodes(states);
         force.links(links);
@@ -40,7 +48,7 @@
                 return colour(i);
             })
 
-        //build the arrow.
+        // build the arrow.
         svg.append("defs")
           .append("marker")
             .attr("id", "end")
@@ -79,8 +87,54 @@
         force.start();
     }
 
-    self.update = function (data) {
+    // adds all func and var names to funcsChosen and varsChosen respectively
+    function setChosenNames(){
+        links.forEach(function (d){
+            funcsChosen.push(d.name);
+        });
+
+        // adds all variable names (that appear in states) to varsChosen
+        for (var name in states[0]){
+            varsChosen.push(name);
+        }
+    }
+
+    // update the layout data
+    self.updateData = function (data) {
         states = data.states;
         links = data.links;
-    }
+
+        force.nodes(states);
+        force.links(links);
+
+        // should start?
+        force.start();
+    };
+
+    self.showFunctionNames = function (){
+        svg.select("defs")
+            .selectAll(".func-name")
+            .data(funcsChosen)
+          .enter().append("marker")
+            .attr("id", function (d){ return d; })
+            .attr("class", "func-name")
+            .attr("marker-width", 20)
+            .attr("marker-height", 20)
+            .attr("viewBox", "0 -10 20 20")
+            .attr("orient", "auto")
+          .append("text")
+            .text(function (d){ return d + "()"; });
+
+        svg.selectAll(".link")
+            .attr("marker-mid", function(d){
+                //return "url(#" + d.name + ")";
+                return "url(#end)";
+            });
+    };
+
+    self.hideFunctionNames = function (){
+        svg.selectAll(".func-name").remove();
+        svg.selectAll(".link").attr("marker-mid", "");
+    };
+
 })(automata.viz = automata.viz || {})
