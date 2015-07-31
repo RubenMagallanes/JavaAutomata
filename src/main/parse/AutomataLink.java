846430aacf9c4aa1ecaf6b653a40e0075050eae0
David Sheridan <dpmsheridan@hotmail.com>
2015-07-31 13:19:07 +1200

    Added a new field to AutomataLink which refers to the method's name.

--- src/main/parse/AutomataLink.java
+++ src/main/parse/AutomataLink.java
@@ -3,12 +3,17 @@ package main.parse;
 public class AutomataLink {
 
 	// fields
+	private String methodName;
 	private int source;
 	private int target;
 
 	public AutomataLink(int source, int target){
 		this.source = source;
 		this.target = target;
+	}
+
+	public String getName(){
+		return methodName;
 	}
 
 	public int getSource(){
