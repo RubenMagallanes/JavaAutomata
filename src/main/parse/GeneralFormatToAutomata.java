71016dd3403e724f0a684f959cadd47d7a4c0628
ChrisChin <chinchri1@myvuw.ac.nz>
2015-07-31 15:59:57 +1200

    added filters for the general format to automata
    added filters for the general format to automata
    
    filters - methods and fields

--- src/main/parse/GeneralFormatToAutomata.java
+++ src/main/parse/GeneralFormatToAutomata.java
@@ -11,31 +11,62 @@ public class GeneralFormatToAutomata {
 	
 	Set<AutomataState> states;
 	Set<AutomataLink> links;
-	String fileName;
 	List<String> methods;
 	List<String> fields;
-	public GeneralFormatToAutomata(Set<AutomataState> states, Set<AutomataLink> links, String fileName,List<String> methods, List<String> fields){
+	
+	/**
+	 * Converts the general format of the trace to a json which can be used to draw an automata
+	 * This includes the methods and fields parameter which filters which ones the user wants to display
+	 * @param states
+	 * @param links
+	 * @param methods
+	 * @param fields
+	 */
+	public GeneralFormatToAutomata(Set<AutomataState> states, Set<AutomataLink> links,List<String> methods, List<String> fields){
 		this.states = states;
 		this.links = links;
-		this.fileName = fileName;
 		this.methods = methods;
 		this.fields = fields;
-		
-		
-		parseAutomata();
-		
+		filter();		
 	}
 	
-	public void parseAutomata(){
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();		
-		System.out.print("states:");
-		System.out.println(gson.toJson(states) +",");
-		System.out.println("links:");
-		System.out.println(gson.toJson(links));
-		
+	/**
+	 * Converts the general format of the trace to a json which can be used to draw an automata
+	 * @param states
+	 * @param links
+	 */
+	public GeneralFormatToAutomata(Set<AutomataState> states, Set<AutomataLink> links){
+		this.states = states;
+		this.links = links;	
 	}
-	public static void main(String[] args){
+	
+	private void filter(){
+		for(AutomataState s: states){
+			List <AutomataField> currentfields = s.getFields();
+			List <AutomataField> removefields = new ArrayList <AutomataField>();
+			
+			for(AutomataField currentfield : currentfields){
+				if(!fields.contains(currentfield.getName())){
+					removefields.add(currentfield);
+				}
+			}
+			
+			currentfields.removeAll(removefields);
+		}
 		
+		List <AutomataLink> removelinks = new ArrayList <AutomataLink>();
+		for(AutomataLink l: links){
+			if(!methods.contains(l.getName())){
+				removelinks.add(l);
+			}
+		}
+		links.removeAll(removelinks);
 	}
-	   
+	
+	public String parseAutomata(){
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();	
+		String s = "states:" + gson.toJson(states) +",links:" +gson.toJson(links); 
+		System.out.println(s);
+		return s;		
+	}	   
 }
