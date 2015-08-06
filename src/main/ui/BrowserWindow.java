9411d9012de968a4fc042a83fc09edbdbf1d59be
ruben_laptop <rubenmmm3@gmail.com>
2015-08-07 11:29:49 +1200

    Added BrowserWindow class Browserwindow is an application extender used for testing.

---
+++ src/main/ui/BrowserWindow.java
@@ -0,0 +1,23 @@
+package main.ui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+ 
+ 
+public class BrowserWindow extends Application {
+    private Scene scene;
+    @Override public void start(Stage stage) {
+        // create the scene
+        stage.setTitle("Web View");
+        scene = new Scene(new Browser(),750,500, Color.web("#666970"));
+        stage.setScene(scene);
+        scene.getStylesheets().add("webviewsample/BrowserToolbar.css");        
+        stage.show();
+    }
+ 
+    public static void main(String[] args){
+        launch(args);
+    }
+}
