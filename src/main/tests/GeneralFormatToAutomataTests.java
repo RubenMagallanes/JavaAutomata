a9dad8a174a2d3409cccb5f30ca2570e7227a52e
ChrisChin <chinchri1@myvuw.ac.nz>
2015-08-07 10:48:52 +1200

    Updated tests for GeneralFormatToAutomata

--- src/main/tests/GeneralFormatToAutomataTests.java
+++ src/main/tests/GeneralFormatToAutomataTests.java
@@ -1,25 +1,10 @@
 package main.tests;
 
-import static org.junit.Assert.*;
-
-import java.awt.Desktop;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.script.Invocable;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-
-import jdk.nashorn.api.scripting.ScriptObjectMirror;
-import main.javascript.JavascriptFileWrapper;
 import main.parse.AutomataField;
 import main.parse.AutomataLink;
 import main.parse.AutomataState;
@@ -33,7 +18,10 @@ import com.google.gson.GsonBuilder;
 
 public class GeneralFormatToAutomataTests {
 
-	
+	/**
+	 * This tests GeneralFormatToAutomata without filters
+	 * this test output should be identical to GeneralFormatToAutomata1.json
+	 */
 	@Test
 	public void testJava(){
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
@@ -43,23 +31,22 @@ public class GeneralFormatToAutomataTests {
 		f.add(new AutomataField("string", "name", "chris"));
 		AutomataState s = new AutomataState(f,1);
 		
-		List<AutomataState> automatastates = new ArrayList<AutomataState>();
+		Set<AutomataState> automatastates = new HashSet<AutomataState>();
 		automatastates.add(s);
 		automatastates.add(s);
 		
-		List<AutomataLink> automatalinks = new ArrayList<AutomataLink>();
+		Set<AutomataLink> automatalinks = new HashSet<AutomataLink>();
 		automatalinks.add(l);
 		automatalinks.add(l);		
 		
-//		System.out.print("states:");
-//		System.out.println(gson.toJson(automatastates) +",");
-//		System.out.println("links:");
-//		System.out.println(gson.toJson(automatalinks));
+		GeneralFormatToAutomata g = new GeneralFormatToAutomata(automatastates, automatalinks);		
+//		System.out.println(g.parseAutomata());	
 	}
 	
 	/**
 	 * This test checks whether the filters work
 	 * The json should only include the count field and toString()
+	 * This test output should be identical to GeneralFormatToAutomataTest2.json
 	 */
 	@Test
 	public void testJavaFilter(){
@@ -91,9 +78,6 @@ public class GeneralFormatToAutomataTests {
 		fields.add("count");
 		
 		GeneralFormatToAutomata g = new GeneralFormatToAutomata(automatastates, automatalinks, methods, fields);		
-		System.out.println(g.parseAutomata());
-		
+		System.out.println(g.parseAutomata());		
 	}
-	
-
 }
