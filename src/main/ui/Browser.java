1f4c33fe7cae3715f6907360ec79c809563d2623
ruben_laptop <rubenmmm3@gmail.com>
2015-08-07 13:57:28 +1200

    Added methods for callign javascript in page
    added an executeCommand method that is for use in the program and a
    toggleDisplay method which is used for testing

--- src/main/ui/Browser.java
+++ src/main/ui/Browser.java
@@ -15,59 +15,68 @@ import javafx.scene.web.WebView;
 import main.Main;
 
 class Browser extends Region {
-	 
-    final WebView browser = new WebView();
-    final WebEngine webEngine = browser.getEngine();
-    
-    String fileUrl = "src/web/HelloWorld.html";
-     
-    public Browser() {
-        //apply the styles
-        getStyleClass().add("browser");
 
-        File f = new File(fileUrl);
-        if(!(f.exists() && !f.isDirectory())) {
-        	System.out.println("Error locating file: " + fileUrl + "\n"); 
-        }
-        else {
-        	System.out.println("located " +fileUrl + "\n");
+	final WebView browser = new WebView();
+	final WebEngine webEngine = browser.getEngine();
 
-        	try {
+	String fileUrl = "src/web/HelloWorld.html";
+
+	// String fileUrl = "http://www.google.com";
+	public Browser() {
+		// apply the styles
+		getStyleClass().add("browser");
+
+		//test weather the url we supplied is valid
+		File f = new File(fileUrl);
+		
+		if (!(f.exists() && !f.isDirectory())) {
+			System.out.println("Error locating file: " + fileUrl);
+		} else {
+			System.out.println("located " + fileUrl );
+			System.out.println("attempting to load page");
+
+			try {
 				webEngine.load(f.toURI().toURL().toString());
 			} catch (MalformedURLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-        }
+		}
+		// add the web view to the scene
+		getChildren().add(browser);
 
-        
-        
-        //add the web view to the scene
-        if(webEngine.getDocument() == null){
-        	System.out.println("document failed to load");
-        } else {
-        	getChildren().add(browser);
-        }
-        
- 
-    }
-    private Node createSpacer() {
-        Region spacer = new Region();
-        HBox.setHgrow(spacer, Priority.ALWAYS);
-        return spacer;
-    }
- 
-    @Override protected void layoutChildren() {
-        double w = getWidth();
-        double h = getHeight();
-        layoutInArea(browser,0,0,w,h,0, HPos.CENTER, VPos.CENTER);
-    }
- 
-    @Override protected double computePrefWidth(double height) {
-        return 750;
-    }
- 
-    @Override protected double computePrefHeight(double width) {
-        return 500;
-    }
+	}
+	
+	public Object executeCommand(String command){
+		if(!webEngine.isJavaScriptEnabled())
+			return null;
+		
+		webEngine.executeScript(command);
+		return null;
+	}
+	public void toggleDisplay(){
+		webEngine.executeScript("document.getElementById('body').innerHTML = 'changed'");
+	}
+
+	private Node createSpacer() {
+		Region spacer = new Region();
+		HBox.setHgrow(spacer, Priority.ALWAYS);
+		return spacer;
+	}
+
+	@Override
+	protected void layoutChildren() {
+		double w = getWidth();
+		double h = getHeight();
+		layoutInArea(browser, 0, 0, w, h, 0, HPos.CENTER, VPos.CENTER);
+	}
+
+	@Override
+	protected double computePrefWidth(double height) {
+		return 750;
+	}
+
+	@Override
+	protected double computePrefHeight(double width) {
+		return 500;
+	}
 }
