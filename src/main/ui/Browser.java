ffeb7cb0c846e05b4fe8754c5731226536a7bc97
ruben_laptop <rubenmmm3@gmail.com>
2015-08-07 12:17:54 +1200

    working on getting locak html file to load.
    web engine can load external pages but has trouble with local ones for
    some reason.

--- src/main/ui/Browser.java
+++ src/main/ui/Browser.java
@@ -1,5 +1,9 @@
 package main.ui;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javafx.geometry.HPos;
 import javafx.geometry.VPos;
 import javafx.scene.Node;
@@ -8,21 +12,43 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import main.Main;
 
 class Browser extends Region {
 	 
     final WebView browser = new WebView();
     final WebEngine webEngine = browser.getEngine();
+    
+    String fileUrl = "src/web/HelloWorld.html";
      
     public Browser() {
         //apply the styles
         getStyleClass().add("browser");
-        // load the web page
-        //webEngine.load("http://www.oracle.com/products/index.html");
-        //webEngine.load("file://src/web/index.html");
-        webEngine.load("File://src/web/HelloWorld.html");
+
+        File f = new File(fileUrl);
+        if(!(f.exists() && !f.isDirectory())) {
+        	System.out.println("Error locating file: " + fileUrl + "\n"); 
+        }
+        else {
+        	System.out.println("located " +fileUrl + "\n");
+
+        	try {
+				webEngine.load(f.toURI().toURL().toString());
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+
+        
+        
         //add the web view to the scene
-        getChildren().add(browser);
+        if(webEngine.getDocument() == null){
+        	System.out.println("document failed to load");
+        } else {
+        	getChildren().add(browser);
+        }
+        
  
     }
     private Node createSpacer() {
