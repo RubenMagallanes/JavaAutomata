790f3c2f97e94d507e02d91e1fe9d380c87d935c
David Sheridan <dpmsheridan@hotmail.com>
2015-08-14 13:58:55 +1200

    Refactored JSONToAutomata to remove a lot of unnecessary code. Also added javadocs to all the methods.

--- src/main/parse/JSONToAutomata.java
+++ src/main/parse/JSONToAutomata.java
@@ -1,33 +1,95 @@
 package main.parse;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
-import java.util.regex.Pattern;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import com.sun.codemodel.internal.JArray;
-
+/**
+ * Static class which is used to convert either a {@code String} of JSON data
+ * or a {@code File} containing JSON data into an {@code Automata}.
+ *
+ * @author David Sheridan
+ *
+ */
 public class JSONToAutomata {
 
+	/**
+	 * {@code String} representing the "enterMethod" JSON key.
+	 */
 	private static final String ENTER_METHOD = "enterMethod";
+
+	/**
+	 * {@code String} representing the "exitMethod" JSON key.
+	 */
 	private static final String EXIT_METHOD = "exitMethod";
+
+	/**
+	 * {@code String} representing the "methodName" JSON key.
+	 */
 	private static final String METHOD_NAME = "methodName";
+
+	/**
+	 * {@code String} representing the "state" JSON key.
+	 */
 	private static final String STATE = "state";
+
+	/**
+	 * Parses the specified {@code String} containing JSON data and converts that
+	 * into an {@code Automata}.
+	 *
+	 * @param json
+	 * 		string containing json data
+	 * @return
+	 * 		automata representing trace
+	 * @throws JSONToAutomataException
+	 * 		thrown if format of json string is invalid
+	 */
+	public static Automata generateAutomata(String json) throws JSONToAutomataException{
+		JSONArray data = new JSONArray(json);
+
+		Set<AutomataState> states = new HashSet<AutomataState>();
+		Set<AutomataLink> links = new HashSet<AutomataLink>();
+		int id = 0;
+
+		AutomataState previous = null;
+		for(int i = 0; i < data.length(); i++){
+			JSONObject object = data.getJSONObject(i);
+			AutomataState current = null;
+
+			// parse the current state
+			if(object.has(ENTER_METHOD)){
+				current = parseState(object.getJSONObject(ENTER_METHOD), id);
+			}
+			else if(object.has(EXIT_METHOD)){
+				current = parseState(object.getJSONObject(EXIT_METHOD), id);
+			}
+			else{
+				throw new JSONToAutomataException("Error: expecting either " + ENTER_METHOD + " or " + EXIT_METHOD + " as key.");
+			}
+			if(!states.contains(current)){
+				states.add(current);
+				id++; // increment id to ensure unique value
+			}
+
+			// construct link between the current and previous states
+			if(previous != null){
+				String methodName = parseMethodName(object);
+				links.add(new AutomataLink(methodName, previous.getId(), current.getId()));
+			}
+
+			// set current state to be previous for next iteration
+			previous = current;
+		}
+
+		return new Automata(states, links);
+	}
 
 	/**
 	 * Parses the specified JSON file and converts that into an {@code Automata}.
@@ -36,88 +98,70 @@ public class JSONToAutomata {
 	 * 		json file containing trace
 	 * @return
 	 * 		automata representing trace
+	 * @throws JSONToAutomataException
+	 * 		thrown if specified file is invalid
 	 */
-	public static Automata generateAutomata(File file){
+	public static Automata generateAutomata(File file) throws JSONToAutomataException{
 		String json = JSONFileToString(file);
-		JSONArray data = new JSONArray(json);
-
-		Set<AutomataState> states = new HashSet<AutomataState>();
-		Set<AutomataLink> links = new HashSet<AutomataLink>();
-		int id = 0;
-
-		for(int i = 0; i < data.length(); i++){
-			JSONObject object = data.getJSONObject(i);
-
-			// edge cases where method entry and exit don't match up
-			if(i == 0 || i == data.length() - 1){
-				if(object.has(ENTER_METHOD)){
-					AutomataState source = parseState(object.getJSONObject(ENTER_METHOD), id);
-					states.add(source);
-					id++;
-				}
-				else if(object.has(EXIT_METHOD)){
-					AutomataState target = parseState(object.getJSONObject(EXIT_METHOD), id);
-					if(!states.contains(target)){
-						states.add(target);
-					}
-				}
-			}
-			else{
-				// parse states
-				AutomataState source = parseState(object.getJSONObject(ENTER_METHOD), id);
-				// only add if source is a unique state
-				if(!states.contains(source)){
-					states.add(source);
-					id++; // increment id
-				}
-				AutomataState target = parseState(data.getJSONObject(++i).getJSONObject(EXIT_METHOD), id);
-				// only add if target is a unique state
-				if(!states.contains(target)){
-					states.add(target);
-					id++; // increment id;
-				}
-
-				// construct link between states
-				String methodName = (object.has(ENTER_METHOD)) ? object.getJSONObject(ENTER_METHOD).getString(METHOD_NAME) :
-					object.getJSONObject(EXIT_METHOD).getString(METHOD_NAME);
-				links.add(new AutomataLink(methodName, source.getId(), target.getId()));
-			}
-		}
-
-		return new Automata(states, links);
+		return generateAutomata(json);
 	}
 
+	/**
+	 * Parses the {@code AutomataState} from the specified {@code JSONObject}.
+	 *
+	 * @param object
+	 * 		the json object to be parsed
+	 * @param id
+	 * 		unique id for state
+	 * @return
+	 * 		{@code AutomataState}
+	 */
 	private static AutomataState parseState(JSONObject object, int id){
 		JSONObject state = object.getJSONObject(STATE);
 		List<AutomataField> fields = new ArrayList<AutomataField>();
+
 		for(String key : state.keySet()){
-			//Object o = state.stringToValue(state.optString(key));
-			//System.out.println(o);
 			String value = (state.isNull(key)) ? null : state.optString(key);
 			fields.add(new AutomataField(null, key, value));
 		}
-
 		return new AutomataState(fields, id);
 	}
 
-	private static String JSONFileToString(File file){
-		try {
-			Scanner scan = new Scanner(file);
-			String json = "";
-			while(scan.hasNextLine()){
-				json += scan.nextLine();
-			}
-			return json;
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	/**
+	 * Parses the method name from the specified {@code JSONObject}.
+	 *
+	 * @param object
+	 * 		json object to be parsed
+	 * @return
+	 * 		method name
+	 */
+	private static String parseMethodName(JSONObject object){
+		if(object.has(ENTER_METHOD)){
+			return object.getJSONObject(ENTER_METHOD).getString(METHOD_NAME);
 		}
-		return null;
+		return object.getJSONObject(EXIT_METHOD).getString(METHOD_NAME);
 	}
 
-	public static void main(String[] args){
-		File file = new File("data/traces/bobby.json");
-		Automata a = generateAutomata(file);
-		System.out.println(a);
+	/**
+	 * Converts the specified JSON file into a {@code String} value and returns it.
+	 *
+	 * @param file
+	 * 		json file to convert
+	 * @return
+	 * 		string representation of json file
+	 * @throws JSONToAutomataException
+	 * 		thrown if file is invalid
+	 */
+	private static String JSONFileToString(File file) throws JSONToAutomataException{
+		Scanner scan = null;
+		try{
+			scan = new Scanner(file);
+		}catch(FileNotFoundException e){throw new JSONToAutomataException("FileNotFoundException: " + e);}
+		String json = "";
+		while(scan.hasNextLine()){
+			json += scan.nextLine();
+		}
+		scan.close();
+		return json;
 	}
 }
---
+++ src/main/parse/JSONToAutomataException.java
@@ -0,0 +1,10 @@
+package main.parse;
+
+public class JSONToAutomataException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+
+	public JSONToAutomataException(String message){
+		super(message);
+	}
+}
