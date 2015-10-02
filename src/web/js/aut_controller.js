ce672e808eeda8aae314a9dfbbcc943ba2b841bb
willhs <whardwicksmith@gmail.com>
2015-10-02 16:25:05 +1300

    Controller fix

--- src/web/js/aut_controller.js
+++ src/web/js/aut_controller.js
@@ -1,20 +1,5 @@
 "use strict";
 
-// get automata data from server
-// and use data to start viz
-$.ajax({
-  type: 'GET',
-  url: url,
-  contentType: 'application/json',
-  success: function(data) {
-      viz.automata.init(JSON.stringify(data, $("div#automata"));
-  },
-  error: function(XMLHttpRequest, textStatus, errorThrown) {
-      console.log("Status: " + textStatus);
-      console.log("Error: " + errorThrown);
-  }
-});
-
 var temp = window.location.href;
 temp = temp.replace("automata=", "TraceRequest/")
 load(temp);
