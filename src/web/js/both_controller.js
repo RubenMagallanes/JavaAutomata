51c9850b274a3bd72f205898537928318a3a8198
ruben_laptop <rubenmmm3@gmail.com>
2015-10-16 13:08:18 +1300

    work on adding functionality to click in automate

--- src/web/js/both_controller.js
+++ src/web/js/both_controller.js
@@ -7,6 +7,10 @@ temp = temp.replace("automata&petri_net=", "TraceRequest/");
 var algoritum = temp.substring(temp.indexOf('?')+1);
 load(temp);
 
+
+//var noderefPetri = viz.petri.getNode;
+
+
 function load(url){
     $.ajax({
         type: 'GET',
@@ -21,8 +25,11 @@ function load(url){
             else if (algoritum.indexOf('normal') >= 0){
                 json = JSON.stringify(data);
             }
-            console.log(json);
+           // console.log(json);
+			//set flag then load, or load then call method
             viz.automata.init(json, $("div#automata"));
+			viz.automata.addMousel();
+			
             viz.petri.init(json, $("div#petrinet"));
         },
         error: function(XMLHttpRequest, textStatus, errorThrown) {
@@ -32,6 +39,10 @@ function load(url){
     });
 }
 
+//define onhover for both
+//viz.automata && viz.petri
+//.on("mouseenter",
+//viz.automata.node.on("mouseclick");
 
 //viz.automata.init(JSON.stringify(testSimpleMonkeyData), $("div#automata"));
 //viz.petri.init(JSON.stringify(testSimpleMonkeyData), $("div#petrinet"));
