5357cf0f122149641317fc61ae9c9bbe5562eb84
David Sheridan <dpmsheridan@hotmail.com>
2015-10-16 14:29:12 +1300

    Refactored and documented ObjectState.

--- src/main/tracer/state/ObjectState.java
+++ src/main/tracer/state/ObjectState.java
@@ -2,10 +2,12 @@ package main.tracer.state;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -13,64 +15,93 @@ import org.json.JSONObject;
 import main.tracer.FieldKey;
 import main.tracer.TraceFilter;
 
+/**
+ * A tracer state representing an {@code Object} in Java.
+ */
 public class ObjectState extends State {
 
 	private static final long serialVersionUID = 1L;
-
 	private static final String NAME = "name";
 	private static final String VALUE = "value";
 
-	//The name of the class
-	private String className;
-
-	//A map of all the fields the class holds
-	private Map<FieldKey, State> fields = new HashMap<>();
-
-
+	// fields
+	private String className; // the name of the class
+	private Map<FieldKey, State> fields = new HashMap<>(); // a map of all the fields the class holds
 	private Map<FieldKey, State> originalFields = new HashMap<>();
 
+	/**
+	 * Constructs a new instance of {@code ObjectState} with the specified
+	 * class name.
+	 *
+	 * @param className
+	 * 		- name of class
+	 */
 	public ObjectState(String className) {
 		this.className = className;
 	}
 
-
-	@Override
-	public String toString() {
-		Map<State, String> alreadySeenObjects = new IdentityHashMap<State, String>();
-
-		if(alreadySeenObjects.containsKey(this)){
-			return alreadySeenObjects.get(this);
-		}
-		alreadySeenObjects.put(this, "OBJ"+alreadySeenObjects.size());
-
-		StringBuilder result = new StringBuilder();
-		result.append("\"state\": {");
-		List<FieldKey> sortedFields = new ArrayList<FieldKey>(getFields().keySet());
-		Collections.sort(sortedFields);
-
-		boolean first = true;
-		for(FieldKey fk : sortedFields) {
-			if(first){
-				first = false;
-			}
-			else{
-				result.append(',');
-			}
-
-			State value = fields.get(fk);
-			result.append("\n\t\t");
-			result.append("\"" + fk.getName() + "\": ");
-			result.append(value.toString(alreadySeenObjects));
-		}
-
-		result.append("\n      }\n");
-
-		return result.toString();
+	/**
+	 * Returns the class name of the {@code Object} represented by
+	 * this {@code ObjectState}.
+	 *
+	 * @return
+	 * 		- class name of object
+	 */
+	public String getClassName() {
+		return className;
 	}
 
+	/**
+	 * Returns the fields of the {@code Object} represented by this
+	 * {@code ObjectState}.
+	 *
+	 * @return
+	 * 		- fields of object
+	 */
+	public Map<FieldKey, State> getFields() {
+		return fields;
+	}
 
 	/**
-	 * Returns a JSON object representing the object
+	 * Sets the fields associated with this {@code ObjectState} with the specified
+	 * fields.
+	 *
+	 * @param fields
+	 * 		- fields to assign
+	 */
+	public void setFields(Map<FieldKey, State> fields) {
+		for (Map.Entry<FieldKey, State> m : fields.entrySet()){
+			originalFields.put(m.getKey(), m.getValue());
+		}
+		this.fields = fields;
+	}
+
+	/**
+	 * Filter the visible fields of this {@code ObjectState} based on the
+	 * specified {@code TraceFilter}.
+	 */
+	public void filterFields(TraceFilter f) {
+		//System.out.println(origonalFields);
+		fields.clear();
+
+		for (Map.Entry<FieldKey, State> m : originalFields.entrySet()){
+			fields.put(m.getKey(), m.getValue());
+		}
+
+		ArrayList<Map.Entry<FieldKey, State>> array = new ArrayList<>(getFields().entrySet());
+
+		for(int i = 0; i < array.size(); i++){
+			if(!f.isFieldTraced(array.get(i).getKey())){
+				fields.remove(array.get(i).getKey());//new
+			}
+			else {
+				array.get(i).getValue().filterFields(f);
+			}
+		}
+	}
+
+	/**
+	 * Returns a JSON object representing this {@code ObjectState}.
 	 * */
 	public JSONArray toJSON(){
 		JSONArray state = new JSONArray();
@@ -88,55 +119,6 @@ public class ObjectState extends State {
 		return state;
 	}
 
-
-	@Override
-	public void filterFields(TraceFilter f) {
-		//System.out.println(origonalFields);
-		fields.clear();
-		for (Map.Entry<FieldKey, State> m : originalFields.entrySet()){
-			fields.put(m.getKey(), m.getValue());
-		}
-		ArrayList<Map.Entry<FieldKey, State>> array = new ArrayList<>(getFields().entrySet());
-
-		for(int i = 0; i < array.size(); i++){
-			if(!f.isFieldTraced(array.get(i).getKey())){
-				System.out.println(array.get(i).getKey());
-				fields.remove(array.get(i).getKey());//new
-			}
-			else {
-				array.get(i).getValue().filterFields(f);
-			}
-		}
-	}
-
-
-	/**
-	 * Returns the class name of the object
-	 * */
-	public String getClassName() {
-		return className;
-	}
-
-
-	/**
-	 * Returns the fields of the object
-	 * */
-	public Map<FieldKey, State> getFields() {
-		return fields;
-	}
-
-
-	/**
-	 * Sets the fields of the object
-	 * */
-	public void setFields(Map<FieldKey, State> fields) {
-		for (Map.Entry<FieldKey, State> m : fields.entrySet()){
-			originalFields.put(m.getKey(), m.getValue());
-		}
-		this.fields = fields;
-	}
-
-
 	@Override
 	public int hashCode() {
 		return 0; // TODO
@@ -145,5 +127,54 @@ public class ObjectState extends State {
 	@Override
 	public boolean equals(Object obj) {
 		return obj instanceof ObjectState && ((ObjectState)obj).getFields().equals(getFields());
+	}
+
+	/**
+	 * Returns a {@code String} representation of this {@code ObjectState}.
+	 */
+	public String toString() {
+		return toString(new IdentityHashMap<State, String>());
+	}
+
+	/**
+	 * Returns a {@code String} representation of this {@code ObjectState}
+	 * based on whether this {@code ObjectState} has already been seen or not.
+	 */
+	public String toString(Map<State, String> alreadySeenObjects) {
+
+		// check if this object has already been seen
+		if(alreadySeenObjects.containsKey(this)){
+			return alreadySeenObjects.get(this);
+		}else{
+			// otherwise add this object to already seen objects
+			alreadySeenObjects.put(this, "OBJ" + alreadySeenObjects.size());
+		}
+
+		StringBuilder result = new StringBuilder();
+		result.append('{');
+
+		// sort the fields
+		List<FieldKey> sortedFields = new ArrayList<FieldKey>(fields.keySet());
+		Collections.sort(sortedFields, new Comparator<FieldKey>() {
+			@Override
+			public int compare(FieldKey o1, FieldKey o2) {
+				return o1.getName().compareTo(o2.getName());
+			}
+		});
+
+		// add fields to the string builder
+		boolean first = true;
+		for(FieldKey fk : sortedFields) {
+			if(first) first = false;
+			else result.append(',');
+
+			State value = fields.get(fk);
+			result.append(fk.getName());
+			result.append('=');
+			result.append(value.toString(alreadySeenObjects));
+		}
+		result.append('}');
+
+		return result.toString();
 	}
 }
