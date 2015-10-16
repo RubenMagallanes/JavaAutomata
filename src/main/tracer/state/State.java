9f2452b78c124ec4cc3ce5421e62c97d3309d11f
David Sheridan <dpmsheridan@hotmail.com>
2015-10-16 15:08:40 +1300

    Documented State and changed so it so that it's an abstract class as that is how it is treated.

--- src/main/tracer/state/State.java
+++ src/main/tracer/state/State.java
@@ -7,21 +7,31 @@ import org.json.JSONArray;
 
 import main.tracer.TraceFilter;
 
-public class State implements Serializable {
+/**
+ * Base state object which other state objects extend.
+ *
+ */
+public abstract class State implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	public static final String OPEN_BRACE = "{";
-	public static final String CLOSE_BRACE = "}";
-	public static final String OPEN_BRACKET = "[";
-	public static final String CLOSE_BRACKET = "]";
-
+	/**
+	 * Filter the visible fields of this {@code State} based on the
+	 * specified {@code TraceFilter}.
+	 */
 	public void filterFields(TraceFilter f) {}
 
+	/**
+	 * Returns a {@code String} representation of this {@code ObjectState}
+	 * based on whether this {@code State} has already been seen or not.
+	 */
 	public String toString(Map<State, String> alreadySeenObjects) {
 		return toString();
 	}
 
+	/**
+	 * Returns a JSON object representing this {@code ObjectState}.
+	 */
 	public JSONArray toJSON(){
 		return new JSONArray();
 	}
